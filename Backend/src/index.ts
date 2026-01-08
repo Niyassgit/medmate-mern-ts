b71cf07d69ce2ea33349d5088c34e53f2c2463e9
Niyas Nazar <niyas28nazar@gmail.com>
2026-01-08 15:38:12 +0530

    vercel.json file added for resolving redierect issue on the frontend

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -5,7 +5,6 @@ import cookieParser from "cookie-parser";
 import path from "path";
 import morgan from "morgan";
 import { env } from "./infrastructure/config/env";
-import { fileURLToPath } from "url";
 import { connectDB } from "./infrastructure/config/db";
 import { LoginRoute } from "./presentation/http/routes/AuthRoute";
 import { MedicalRepRoutes } from "./presentation/http/routes/MedicalRepRoutes";
